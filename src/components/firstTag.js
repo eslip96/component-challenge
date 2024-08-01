445d9516d11bac0945e629a8f82f56b23c8b1f9f
enoka silipa <eslip96@gmail.com>
2024-08-01 00:10:40 -0600

    finished assignment

---
+++ src/components/firstTag.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class FirstTag extends Component {
+  render() {
+    return (
+      <div>
+        <h1>first h1 tag</h1>
+      </div>
+    );
+  }
+}
+
+export default FirstTag;
---
+++ src/components/secondTag.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class SecondTag extends Component {
+  render() {
+    return (
+      <div>
+        <h2>my second H2 Tag</h2>
+      </div>
+    );
+  }
+}
+
+export default SecondTag;
